be4f1a7fb437ae186fb0c2a3ca25554b6b617d7e
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-10-08 11:04:46 +0300

    Implement start page parser

--- dataset-scrapper/scrapper.py
+++ dataset-scrapper/scrapper.py
@@ -1,12 +1,21 @@
-from typing import Dict, List
+import logging
+import re
+from typing import Any, Dict, List
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 SCHEMA_HABR = {
+    'domain': 'habr.com',
     'article': 'article.tm-articles-list__item',
     'title': '.tm-article-snippet__title span',
+    'preview': '.tm-article-body',
+    'full_article': '.article-formatted-body',
+    'tag': '.tm-article-snippet__hubs-item',
+    'link': '.tm-article-snippet__title-link',
 }
+
+space_replacer = re.compile(r'\s+')
 
 
 def get_page_content(url: str) -> str:
@@ -20,8 +29,50 @@ def parse_page(schema: Dict[str, str], contents: List[str]) -> List[Dict[str, st
     for content in contents:
         soup = BeautifulSoup(content, 'lxml')
         for art in soup.select(schema['article']):
-            articles.append(dict(
-                title=art.select(schema['title'])[0].get_text(),
-            ))
+            art_title = get_node_text(art.select(schema['title']))
+            logging.info(f'Parse {art_title}')
+
+            art_url = parse_link(schema['domain'], art.select(schema['link']))
+
+            art_text = ''
+            art_data = dict(
+                title=art_title,
+                preview=get_node_text(art.select(schema['preview'])),
+                article=art_text,
+                tags=parse_tags(art.select(schema['tag'])),
+                link=art_url,
+            )
+            articles.append(art_data)
+
+            if art_url:
+                resp = requests.get(art_url)
+                if resp.ok:
+                    art_text = get_node_text(BeautifulSoup(resp.text, 'lxml').select(schema['full_article']))
+                    art_data['article'] = art_text
 
     return articles
+
+
+def get_node_text(result_set: Any) -> str:
+    if len(result_set):
+        item = result_set[0]
+        texsts = item.findAll(text=lambda x: isinstance(x, NavigableString))
+        return space_replacer.sub(' ', ' '.join(texsts).strip())
+    return ''
+
+
+def parse_tags(result_set: Any) -> List[str]:
+    result = []
+    for item in result_set:
+        texsts = item.findAll(text=lambda x: isinstance(x, NavigableString))
+        result.append(' '.join(texsts).strip())
+    return result
+
+
+def parse_link(domain: str, result_set: Any) -> str:
+    if len(result_set) == 0:
+        return ''
+    url = result_set[0].attrs['href']
+    if url.startswith('/'):
+        return f'https://{domain}{url}'
+    return url
