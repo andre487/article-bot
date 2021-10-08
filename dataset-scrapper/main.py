c861aef6cdb6b19c35fecac0019a1f6906029d9c
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2021-10-08 11:34:18 +0300

    Implement saving

--- dataset-scrapper/main.py
+++ dataset-scrapper/main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import time
 
@@ -14,18 +15,20 @@ def main():
 @main.command()
 @click.option('--start-page', default='https://habr.com/ru/all/top100/', show_default=True)
 @click.option('--page-count', default=10, show_default=True)
-def habr(start_page: str, page_count: int):
+@click.argument('result_file', required=True)
+def habr(start_page: str, page_count: int, result_file: str):
     contents = []
     for i in range(1, page_count + 1):
         logging.info(f'Get page {i} content')
         content = scrapper.get_page_content(start_page + f'page{i}/')
         contents.append(content)
-        time.sleep(0.1)
+        time.sleep(0.25)
 
     logging.info('Parse pages')
     parsed_page = scrapper.parse_page(scrapper.SCHEMA_HABR, contents)
 
-    print(parsed_page)
+    with open(result_file, 'w') as fp:
+        json.dump(parsed_page, fp, indent=2, ensure_ascii=False)
 
 
 
